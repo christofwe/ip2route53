b0f3d3ba60bb39d7ae9b80aef6e72ccb2d029b5f
Christof Weigl <christof.weigl@gmail.com>
2022-03-05 07:54:17 +0100

    write to influxdb using v2 API

--- ip2route53.py
+++ ip2route53.py
@@ -1,19 +1,32 @@
 import os
 from requests import get
 
-from datetime import datetime
+from datetime import datetime, timedelta
+import pytz
 
 import boto3
 from botocore.exceptions import ClientError
+
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
 
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_DEFAULT_REGION = os.environ['AWS_DEFAULT_REGION']
 HOSTED_ZONE_ID = os.environ['HOSTED_ZONE_ID']
 DOMAIN_NAME = os.environ['DOMAIN_NAME']
+INFLUXDB_URL = os.environ['INFLUXDB_URL']
+INFLUXDB_TOKEN = os.environ['INFLUXDB_TOKEN']
+INFLUXDB_ORG = os.environ['INFLUXDB_ORG']
+INFLUXDB_BUCKET = os.environ['INFLUXDB_BUCKET']
 
-local = datetime.now()
-timestamp = local.strftime("%Y-%m-%dT%H:%M:%SZ")
+tz = pytz.timezone(os.environ['TZ'])
+local = tz.localize(datetime.now())
+timestamp = local.strftime("%Y-%m-%dT%H:%M:%S%Z%z")
+
+influx_client = InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG)
+influx_write_api = influx_client.write_api(write_options=SYNCHRONOUS)
+
 
 external_ip = get('https://api.ipify.org').content.decode('utf8')
 
@@ -30,7 +43,7 @@ try:
           'ResourceRecordSet': {
             'Name': DOMAIN_NAME,
             'Type': 'A',
-            'TTL': 900,
+            'TTL': 28800,
             'ResourceRecords': [
               {
                 'Value': external_ip
@@ -46,3 +59,9 @@ try:
 
 except ClientError as client_error:
   print(f"{timestamp} ClientError: {client_error}")
+
+
+data_point = Point("ip_address").tag("type", "report").field("ipv4", external_ip)
+
+influx_write_api.write(bucket=INFLUXDB_BUCKET, record=data_point)
+influx_client.close()
